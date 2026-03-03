9489c8dea25faafcc0f59a3b3ad179d6a2ed9782
Johnmclf <johnmclf@gmail.com>
2026-03-03 08:30:37 +0100

    Implémentation du Docker

---
+++ src/index.php
@@ -0,0 +1,63 @@
+<?php
+
+$isCliServer = (PHP_SAPI === "cli-server");
+$host = getenv("DB_HOST") ?: ($isCliServer ? "127.0.0.1:3306" : "db");
+$db = getenv("DB_NAME") ?: "myapp_db";
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "rootpassword";
+$charset = getenv("DB_CHARSET") ?: "utf8mb4";
+
+try {
+    $p = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+
+    $p->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $p->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo 'Erreur lors de la connexion à la BDD : ' . $e->getMessage();
+    exit();
+}
+
+try {
+    $d = $p->query("SELECT id,text FROM db_table")->fetchAll(PDO::FETCH_ASSOC);
+} catch (Exception $e) {
+    $p->prepare('CREATE TABLE IF NOT EXISTS db_table (id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(100) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4')->execute();
+    $p->prepare('INSERT INTO db_table (text) VALUES (:text)')->execute([':text' => 'azerty']);
+    $p->prepare('INSERT INTO db_table (text) VALUES (:text)')->execute([':text' => 'abcdef']);
+    $p->prepare('INSERT INTO db_table (text) VALUES (:text)')->execute([':text' => 'xyz']);
+    $p->prepare('INSERT INTO db_table (text) VALUES (:text)')->execute([':text' => '123456789']);
+    $d = $p->query('SELECT id,text FROM db_table')->fetchAll(PDO::FETCH_ASSOC);
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>R6.06 Maintenance applicative</title>
+</head>
+<body>
+<header>
+    <h1>R6.06 Maintenance applicative</h1><h2 style="color: crimson">Evaluation</h2>
+    <p style="color: crimson">Modifiez ce projet à l'aide des outils vus ensemble pour améliorer la maintenabilité de ce projet et déployez le sur le serveur mis à votre disposition</p><p style="color: crimson">Vous êtes libre de modifier ce que vous souhaitez sur le projet, chaque amélioration (ou début d'amélioration) sera prise en compte dans la notation</p>
+    <p style="color: crimson; font-weight: bold; border: solid 2px crimson; padding: 5px; width: fit-content;">Pensez à inviter cdiiv sur votre projet Github</p>
+</header>
+
+<table>
+    <thead style="font-weight: bold;">
+        <tr>
+            <td style="border: solid black 1px">Id</td>
+            <td style="border: solid black 1px">Text</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $i=0; while (true) { if(!key_exists($i, $d)) break;?>
+            <tr>
+                <td style="border: solid black 1px"><?= $d[$i]['id'] ?></td>
+                <td style="border: solid black 1px"><?= $d[$i]['text'] ?></td>
+            </tr>
+        <?php $i++; } ?>
+    </tbody>
+</table>
+</body>
+</html>
