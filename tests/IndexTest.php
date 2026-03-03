8495303d1d2a97961801d3e17929dd6caf7c47cd
Johnmclf <johnmclf@gmail.com>
2026-03-03 10:17:30 +0100

    Ajout de commentaire

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -6,6 +6,7 @@ class IndexTest extends TestCase
 {
     public function testIndexPageRendersCorrectly()
     {
+        // Pour debug
         putenv("DB_HOST=db");
         putenv("DB_NAME=myapp_db");
         putenv("DB_USER=user");
@@ -16,14 +17,17 @@ class IndexTest extends TestCase
         include __DIR__ . '/../src/index.php';
         $output = ob_get_clean();
 
+        // Vérifie que les données de la table sont présentes dans le rendu HTML
         $this->assertStringContainsString('Id', $output);
         $this->assertStringContainsString('Text', $output);
 
+        // Vérifie que les données insérées sont présentes dans le rendu HTML
         $this->assertStringContainsString('azerty', $output);
         $this->assertStringContainsString('abcdef', $output);
         $this->assertStringContainsString('xyz', $output);
         $this->assertStringContainsString('123456789', $output);
 
+        // Vérifie qu'il n'y a pas d'erreurs de connexion à la base de données ou d'exécution de requêtes
         $this->assertStringNotContainsString('Warning:', $output);
         $this->assertStringNotContainsString('Fatal error:', $output);
     }
